aaa8e57e53e3bc2fc4e754ffda158051a17cffbd
Jérémy <elvynia@gmail.com>
2019-01-09 10:03:21 +0100

    Implémentation du composant principal avec une liste des stocks affichée.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 
+const CHOCO_STOCKS = [
+  {
+    id: 0,
+    stock: 100
+  }, {
+    id: 1,
+    stock: 300
+  }, {
+    id: 2,
+    stock: 0
+  }, {
+    id: 3,
+    stock: 100
+  }, {
+    id: 4,
+    stock: 100
+  }
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +26,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chocostocks';
+  chocoStocks: Array<any>;
+
+  constructor() {
+    this.chocoStocks = new Array();
+    this.chocoStocks.push(...CHOCO_STOCKS);
+  }
 }
